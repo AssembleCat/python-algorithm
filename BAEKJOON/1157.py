da174369e6127b82e5791fdacb776e33240329f9
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2025-04-28 18:00:17 +0900

    1157번 새로알게된점 정리

--- BAEKJOON/1157.py
+++ BAEKJOON/1157.py
@@ -1,6 +1,10 @@
 """
 문제
 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+
+TODO(string.lower(), upper()로 대소문자로 구분가능)
+TODO(string.islower(), isupper()로 대소문자 여부 확인가능 모든 문자가 소문자, 대문자 일때만 True ex) temp = "TETE" 일때 temp.isupper()는 True)
+TODO(dict에 접근할때 dict.get(value, 0)으로 기본값을 지정할 수 있음.)
 """
 
 word = input().upper()
