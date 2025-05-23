62fc005554d443917ca3234fc48bc35a8751457d
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2025-05-23 10:46:38 +0900

    Title: 이상한 다트 게임, Time: 127ms, Memory: 21MB, Status: Passed - Codetree

--- 250523/이상한 다트 게임/odd-dart-game.py
+++ 250523/이상한 다트 게임/odd-dart-game.py
@@ -1,3 +1,5 @@
+import math
+
 class Board():
     def __init__(self, grid, n, m):
         self.grid = grid
@@ -7,15 +9,12 @@ class Board():
     def rotation(self, x, d, k):
         # 원판 대상, 회전 방향, 회전 칸수를 받아 회전을 실행시킴 0 - 시계, 1 - 반시계
         target_idx = x - 1
-        #print(f"({x, d, k}) before {self.grid}")
         while target_idx < len(self.grid):
-            #print(f"target_idx: {target_idx}")
             if d == 0: # 시계 방향 회전 [1, 2, 3, 4] -> [4, 1, 2, 3]
                 self.grid[target_idx] = self.grid[target_idx][self.m - k:] + self.grid[target_idx][:self.m - k]
             else:
                 self.grid[target_idx] = self.grid[target_idx][k:] + self.grid[target_idx][:k] 
             target_idx += x
-        #print(f"({x, d, k}) after {self.grid}")
 
     def in_range(self, x):
         return 0 <= x < self.n
@@ -40,13 +39,26 @@ class Board():
         
         for i, j in list(remove_target):
             self.grid[i][j] = -1
+        
+        return len(remove_target) > 0
 
     def generalized(self):
         total = self.get_total()
-        avg = math.floor(total / (self.n * self.m))
-        #print(f"{total, avg}")
+        count = sum([
+            1
+            for i in range(self.n)
+            for j in range(self.m)
+            if self.grid[i][j] != -1
+        ])
+
+        if count == 0:
+            return 
+
+        avg = total // count
         for i in range(self.n):
             for j in range(self.m):
+                if self.grid[i][j] == -1:
+                    continue
                 if self.grid[i][j] == avg: # 평균과 같으면 스킵 
                     continue
                 if self.grid[i][j] > avg:
@@ -66,10 +78,10 @@ class Board():
 
     def get_total(self):
         total = sum([
-            grid[i][j]
+            self.grid[i][j]
             for i in range(self.n)
             for j in range(self.m)
-            if grid[i][j] != -1
+            if self.grid[i][j] != -1
         ])
 
         return total
@@ -82,9 +94,7 @@ board = Board(grid, n, m)
 for r_type in rotate_type:
     x, d, k = r_type
     board.rotation(x, d, k)
-    board.remove()
-
-if not board.is_removed():
-    board.generalized()
+    if not board.remove():
+        board.generalized()
 
 print(board.get_total())
